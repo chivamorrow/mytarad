88dc0382913a89a8f890a2d20bf99b9eee53a975
Chiva <chivamorrow@gmail.com>
2013-05-22 21:33:08 +0700

    init

---
+++ Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,173 @@
+﻿for(var i = 0; i < 68; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+document.getElementById('u21_img').tabIndex = 0;
+
+u21.style.cursor = 'pointer';
+$axure.eventManager.click('u21', function(e) {
+
+if (true) {
+
+	NewTab('http://nanopc.tarad.com/product.detail_1005784_th_5405182', "");
+
+}
+});
+gv_vAlignTable['u51'] = 'center';gv_vAlignTable['u16'] = 'top';document.getElementById('u46_img').tabIndex = 0;
+
+u46.style.cursor = 'pointer';
+$axure.eventManager.click('u46', function(e) {
+
+if (true) {
+
+	NewTab('http://nanopc.tarad.com/product.detail_1005784_th_5405182', "");
+
+}
+});
+gv_vAlignTable['u31'] = 'top';document.getElementById('u38_img').tabIndex = 0;
+
+u38.style.cursor = 'pointer';
+$axure.eventManager.click('u38', function(e) {
+
+if (true) {
+
+	NewTab('http://nanopc.tarad.com/product.detail_1005784_th_5405182', "");
+
+}
+});
+gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u62'] = 'top';gv_vAlignTable['u53'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u60'] = 'center';gv_vAlignTable['u34'] = 'top';document.getElementById('u17_img').tabIndex = 0;
+
+u17.style.cursor = 'pointer';
+$axure.eventManager.click('u17', function(e) {
+
+if (true) {
+
+	NewTab('http://nanopc.tarad.com/product.detail_1005784_th_5401772', "");
+
+}
+});
+gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u58'] = 'top';document.getElementById('u2_img').tabIndex = 0;
+HookHover('u2', false);
+HookClick('u2', false);
+
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+	BringToFront("u37");
+
+	SetPanelVisibility('u37','','none',500);
+
+	SetPanelVisibility('u8','hidden','none',500);
+
+	SetPanelVisibility('u63','hidden','none',500);
+
+	SetPanelVisibility('u54','hidden','none',500);
+
+SetWidgetSelected('u2');
+                                EnableImageWidget('u6');
+                                EnableImageWidget('u4');
+                                EnableImageWidget('u0');
+}
+});
+gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u52'] = 'top';gv_vAlignTable['u43'] = 'center';document.getElementById('u0_img').tabIndex = 0;
+HookHover('u0', false);
+HookClick('u0', false);
+
+u0.style.cursor = 'pointer';
+$axure.eventManager.click('u0', function(e) {
+
+if (true) {
+
+	BringToFront("u8");
+
+	SetPanelVisibility('u8','','none',500);
+
+	SetPanelVisibility('u63','hidden','none',500);
+
+	SetPanelVisibility('u37','hidden','none',500);
+
+	SetPanelVisibility('u54','hidden','none',500);
+
+SetWidgetSelected('u0');
+                                EnableImageWidget('u6');
+                                EnableImageWidget('u4');
+                                EnableImageWidget('u2');
+}
+});
+gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u39'] = 'center';document.getElementById('u4_img').tabIndex = 0;
+HookHover('u4', false);
+HookClick('u4', false);
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	BringToFront("u54");
+
+	SetPanelVisibility('u54','','none',500);
+
+	SetPanelVisibility('u8','hidden','none',500);
+
+	SetPanelVisibility('u63','hidden','none',500);
+
+	SetPanelVisibility('u37','hidden','none',500);
+
+SetWidgetSelected('u4');
+                                EnableImageWidget('u6');
+                                EnableImageWidget('u0');
+                                EnableImageWidget('u2');
+}
+});
+document.getElementById('u6_img').tabIndex = 0;
+HookHover('u6', false);
+HookClick('u6', false);
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	BringToFront("u63");
+
+	SetPanelVisibility('u63','','none',500);
+
+	SetPanelVisibility('u8','hidden','none',500);
+
+	SetPanelVisibility('u37','hidden','none',500);
+
+	SetPanelVisibility('u54','hidden','none',500);
+
+SetWidgetSelected('u6');
+                                EnableImageWidget('u4');
+                                EnableImageWidget('u0');
+                                EnableImageWidget('u2');
+}
+});
+gv_vAlignTable['u61'] = 'top';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u56'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u12'] = 'top';document.getElementById('u9_img').tabIndex = 0;
+
+u9.style.cursor = 'pointer';
+$axure.eventManager.click('u9', function(e) {
+
+if (true) {
+
+	NewTab('http://nanopc.tarad.com/product.detail_1005784_th_5399951', "");
+
+}
+});
+document.getElementById('u42_img').tabIndex = 0;
+
+u42.style.cursor = 'pointer';
+$axure.eventManager.click('u42', function(e) {
+
+if (true) {
+
+	NewTab('http://nanopc.tarad.com/product.detail_1005784_th_5401772', "");
+
+}
+});
+gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u67'] = 'center';gv_vAlignTable['u57'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u44'] = 'top';
